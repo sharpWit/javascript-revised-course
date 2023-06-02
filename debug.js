b31d1eddf7cf584288fef2cb9f825b7b31ae5dc3
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2023-06-02 19:21:49 +0330

    modified:   debug.js 	modified:   index.html

--- debug.js
+++ debug.js
@@ -1,13 +1,23 @@
+// function debug(msg) {
+//   var log = document.getElementById("debuglog");
+//   if (!log) {
+//     var log = document.createElement("div");
+//     log.id = "debuglog";
+//     log.innerHTML = "<h1> Debug Log </h1>";
+//     document.body.appendChild(log);
+//   }
+//   var pre = document.createElement("pre");
+//   var text = document.createTextNode(msg);
+//   pre.appendChild(text);
+//   log.appendChild(pre);
+// }
+// ? jQuery
 function debug(msg) {
-  var log = document.getElementById("debuglog");
-  if (!log) {
-    var log = document.createElement("div");
-    log.id = "debuglog";
-    log.innerHTML = "<h1> Debug Log </h1>";
-    document.body.appendChild(log);
+  var log = $("#debuglog");
+  if (log.length == 0) {
+    log = $("<div id='debuglog'><h1>Debug Log</h1></div>");
+    log.appendTo(document.body);
   }
-  var pre = document.createElement("pre");
-  var text = document.createTextNode(msg);
-  pre.appendChild(text);
-  log.appendChild(pre);
+  log.append($("<pre/>").text(msg));
 }
+// ?
