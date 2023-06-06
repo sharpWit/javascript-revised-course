5342a446c9ff3bf94e98abb67c1f0f274e739428
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2023-06-06 21:47:46 +0330

    renamed:    loan-calculator.html -> components/loan-calculator/loan-calculator.html 	new file:   components/modal/modal.css 	new file:   components/modal/modal.html 	new file:   components/modal/modal.js 	modified:   index.html 	modified:   index.js 	renamed:    debug.js -> utils/debug.js 	renamed:    hide.js -> utils/hide.js

--- index.js
+++ index.js
@@ -182,3 +182,15 @@
 // }
 // console.log(checkScope());
 // ?
+function test(o) {
+  var i = 0;
+  if (typeof o == "object") {
+    var j = 0;
+    for (var k = 0; k < 10; k++) {
+      console.log(k);
+    }
+    console.log(k);
+  }
+  console.log(j);
+}
+console.log(test());
