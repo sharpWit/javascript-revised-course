ef210f7f376097ecafb017357dee9dbb0443f15b
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2023-06-09 22:09:50 +0330

    modified:   index.js 	new file:   main.js

--- index.js
+++ index.js
@@ -1,271 +1,40 @@
-// ? absolute
-// function abs(x) {
-//   if (x >= 0) {
-//     return x;
-//   } else {
-//     return -x;
-//   }
+"use strict";
+
+// function plus1(x) {
+//   return x + 1;
 // }
-// console.log(abs(-1));
-// ? factorial
-// function factorial(n) {
-//   var product = 1;
-//   while (n > 1) {
-//     product *= n;
-//     n--;
-//   }
-//   return product;
-// }
-// console.log(factorial(4));
-//! another way
-// function factorial2(n) {
-//   var i,
-//     product = 1;
-//   for (i = 2; i <= n; i++) {
-//     product *= i;
-//   }
-//   return product;
-// }
-// console.log(factorial2(5));
-// ? 2D geomatric
-// function Point(x, y) {
-//   this.x = x;
-//   this.y = y;
-// }
-// var p = new Point(1, 1);
-// Point.prototype.r = function () {
-//   return Math.sqrt(this.x * this.x + this.y * this.y);
+// plus1(3);
+// let square = function (x) {
+//   return x * x;
 // };
-// console.log(p.r());
-// ? client-side
-// function moveon() {
-//   var answer = confirm("Ready to move on?");
-//   if (answer) window.location = "http://google.com";
-// }
-// setTimeout(moveon, 5000);
-// ? Create an element
-// function debug(msg) {
-//   var log = document.getElementById("debuglog");
-//   if (!log) {
-//     var log = document.createElement("div");
-//     log.id = "debuglog";
-//     log.innerHTML = "<h1> Debug Log </h1>";
-//     document.body.appendChild(log);
-//   }
-//   var pre = document.createElement("pre");
-//   var text = document.createTextNode(msg);
-//   pre.appendChild(text);
-//   log.appendChild(pre);
-// }
-// debug("This is a 'pre' debugLog");
-// ? hide element
-// function hide(e, reflow) {
-//   if (reflow) {
-//     e.style.display = "none";
-//   } else {
-//     e.style.visibility = "hidden";
-//   }
-// }
 
-// function highlight(e) {
-//   if (!e.className) e.className = "hilite";
-//   else e.className += "hilite";
-// }
-// ? hide images element
-// window.onload = function () {
-//   var images = document.getElementsByTagName("img");
-//   for (var i = 0; i < images.length; i++) {
-//     var image = images[i];
-//     if (image.addEventListener) image.addEventListener("click", hide, false);
-//     else image.attachEvent("onclick", hide);
-//   }
-//   function hide(event) {
-//     event.target.style.visibility = "hidden";
-//   }
+// console.log(square(plus1(3)));
+
+// let primes = [2, 3, 5, 7];
+
+// console.log(primes[primes.length - 1]);
+
+// primes.content = {
+//   name: "Saeed",
 // };
-// ? date
-// var then = new Date(2010, 0, 1);
-// var later = new Date(2010, 0, 1, 1, 2, 1);
-// var now = new Date();
-// var elapsed = now - then;
-// console.log(elapsed);
-// console.log(later.getFullYear());
-// console.log(later.getMonth());
-// console.log(later.getDate());
-// console.log(later.getDay());
-// console.log(later.getHours());
-// console.log(later.getUTCHours());
-// console.log(later.toString());
-// console.log(later.toUTCString());
-// console.log(later.toLocaleDateString());
-// console.log(later.toLocaleTimeString());
-// console.log(later.toISOString());
-// ? strings
-// var s = "Hello, world";
-// console.log(
-//   s.charAt(0),
-//   s.charAt(s.length - 1),
-//   s.substring(1, 4),
-//   s.slice(-3),
-//   s.slice(1, 4),
-//   s.indexOf("1"),
-//   s.lastIndexOf("1"),
-//   s.indexOf("1", 3),
-//   s.split(","),
-//   s.replace("h", "H"),
-//   s.toUpperCase()
-// );
-// ? RegExps
-// var text = "testing: 1, 2, 3";
-// var pattern = /\d+/g;
-// console.log(pattern.test(text));
-// console.log(text.search(pattern));
-// console.log(text.match(pattern));
-// console.log(text.replace(pattern, "#"));
-// console.log(text.split(/\D+/));
-// ? string methods
-// var s = "hello world!";
-// var word = s.substring(s.indexOf("") + 1, s.length);
-// console.log(word);
-// ? object and array equality
-// var a = ["a", "b", "c"];
-// var b = [];
-// for (var i = 0; i < a.length; i++) {
-//   b[i] = a[i];
-// }
-// function equalArrays(a, b) {
-//   if (a.length != b.length) return false;
-//   for (var i = 0; i < a.length; i++) if (a[i] !== b[i]) return false;
-//   return true;
-// }
-// ?
-// var n = 17;
-// console.log((binary_string = n.toString(2)));
-// console.log((octal_string = "0" + n.toString(8)));
-// console.log((hex_string = "0x" + n.toString(16)));
-// ?
-// var n = 123456.789;
-// console.log(n.toFixed(0));
-// console.log(n.toFixed(2));
-// console.log(n.toFixed(5));
-// console.log(n.toExponential(1));
-// console.log(n.toExponential(3));
-// console.log(n.toPrecision(4));
-// console.log(n.toPrecision(7));
-// console.log(n.toPrecision(10));
-// ?
-// var scope = "global";
-// function checkScope() {
-//   var scope = "local";
-//   return scope;
-// }
-// console.log(checkScope());
 
-// scope = "global";
-// function checkScope2() {
-//   scope = "local";
-//   myScope = "local";
-//   return [scope, myScope];
-// }
-// console.log(checkScope2());
-// console.log(scope);
-// console.log(myScope);
+// console.log(primes.content?.name);
 
-// var scope = "global scope";
-// function checkScope() {
-//   var scope = "local scope";
-//   function nested() {
-//     var scope = "nested scope";
-//     return scope;
-//   }
-//   return nested();
-// }
-// console.log(checkScope());
-// ?
-// function test(o) {
-//   var i = 0;
-//   if (typeof o == "object") {
-//     var j = 0;
-//     for (var k = 0; k < 10; k++) {
-//       console.log(k);
-//     }
-//     console.log(k);
-//   }
-//   console.log(j);
-// }
-// console.log(test());
-// ?
-// for (var i = 0; i < 10; i++) console.log(i);
-// for (var i = 0, j = 10; i <= 10; i++, j--) console.log(i * j);
-// for (var i in o) console.log(i);
-//
-// function hypotenuse(x, y) {
-//   return Math.sqrt(x * x + y * y);
-// }
-// console.log(hypotenuse(3, 3));
-// recursive function
-function factorial(n) {
-  if (n <= 1) return 1;
-  return n * factorial(n - 1);
-}
+// console.log(primes.length);
+// console.log(primes.push(13));
+// console.log(primes);
+// console.log(primes.reverse());
+// ? distance between points
+let points = [
+  { x: 0, y: 0 },
+  { x: 1, y: 1 },
+];
 
-// console.log(factorial(3));
-//
-// function condition() {
-//   i = j = 1;
-//   k = 2;
-//   if (i == j) {
-//     if (j == k) {
-//       return console.log("i equals k");
-//     }
-//   } else {
-//     return console.log("i doesn't equal to j");
-//   }
-// }
-// console.log(condition());
-//
-// function convert(x) {
-//   switch (typeof x) {
-//     case "number":
-//       return x.toString(2);
-//     case "string":
-//       return '"' + x + '"';
-//     default:
-//       return String(x);
-//   }
-// }
-
-// console.log(convert(999));
-//
-// var count = 0;
-// while (count < 10) {
-//   console.log(count);
-//   count++;
-// }
-// function printArray(a) {
-//   var len = a.length,
-//     i = 0;
-//   if (len == 0) console.log("Empty array");
-//   else {
-//     do {
-//       console.log(a[i]);
-//     } while (++i < len);
-//   }
-// }
-// printArray(["a", "b", "c"]);
-//
-// function condy() {
-//   var o = { x: 1, y: 2, z: 3 };
-//   var a = [],
-//     i = o;
-//   for (i in a) console.log(i);
-// }
-// console.log(condy());
-//
-try {
-  var n = Number(prompt("Please enter a positive integer", ""));
-  var f = factorial(n);
-  alert(n + "! = " + f);
-} catch (ex) {
-  alert(ex);
-}
+points.dist = function () {
+  let p1 = this[0];
+  let p2 = this[1];
+  let a = p2.x - p1.x;
+  let b = p2.y - p1.y;
+  return Math.sqrt(a * a + b * b); // The Pythagorean theorem
+};
+console.log(points.dist());
