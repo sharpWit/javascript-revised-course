71d5121732c87148093c6e82c4451b3d366ebdce
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2023-06-09 23:22:03 +0330

    new file:   charfreq.js 	modified:   index.js 	new file:   lorem.text

--- index.js
+++ index.js
@@ -25,16 +25,66 @@
 // console.log(primes);
 // console.log(primes.reverse());
 // ? distance between points
-let points = [
-  { x: 0, y: 0 },
-  { x: 1, y: 1 },
-];
+// let points = [
+//   { x: 0, y: 0 },
+//   { x: 1, y: 1 },
+// ];
 
-points.dist = function () {
-  let p1 = this[0];
-  let p2 = this[1];
-  let a = p2.x - p1.x;
-  let b = p2.y - p1.y;
-  return Math.sqrt(a * a + b * b); // The Pythagorean theorem
-};
-console.log(points.dist());
+// points.dist = function () {
+//   let p1 = this[0];
+//   let p2 = this[1];
+//   let a = p2.x - p1.x;
+//   let b = p2.y - p1.y;
+//   return Math.sqrt(a * a + b * b); // The Pythagorean theorem
+// };
+// console.log(points.dist());
+// ? absolute value
+// function abs(x) {
+//   if (x >= 0) {
+//     return x;
+//   } else {
+//     return -x;
+//   }
+// }
+// let equality = abs(-10) === abs(10);
+// console.log(equality);
+// ? sum
+// function sum(array) {
+//   let sum = 0;
+//   for (let x of array) {
+//     sum += x;
+//   }
+//   return sum;
+// }
+// console.log(sum(primes));
+// ? factorial
+// function factorial(n) {
+//   let product = 1;
+//   while (n > 1) {
+//     product *= n;
+//     n--;
+//   }
+//   return product;
+// }
+// console.log(factorial(4));
+// ---------------
+// function factorial2(n) {
+//   let i,
+//     product = 1;
+//   for (i = 2; i <= n; i++) product *= i;
+//   return product;
+// }
+// console.log(factorial2(5));
+// ? 2D geromatrix points
+// class Point {
+//   constructor(x, y) {
+//     this.x = x;
+//     this.y = y;
+//   }
+//   distance() {
+//     return Math.sqrt(this.x * this.x + this.y * this.y);
+//   }
+// }
+// let p = new Point(3, 5);
+// console.log(p.distance());
+// ? Character Frequency Histograms
