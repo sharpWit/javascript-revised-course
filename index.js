c49d1ed8740da0dfed863152029d8cc361baa733
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2023-07-03 03:13:30 +0330

    modified:   index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // addToCart("bread", 5);
 // console.log(price, tq);
 
-console.log("Importing module");
+// console.log("Importing module");
 // console.log(shippingCost);
 
 // import * as ShoppingCart from "./shoppingCart.js";
@@ -13,12 +13,13 @@ console.log("Importing module");
 // console.log(ShoppingCart.totalPrice);
 
 // import add, { addToCart, totalPrice as price, tq } from "./shoppingCart.js";
-import add, { cart } from "./shoppingCart.js";
-add("pizza", 2);
-add("bread", 5);
-add("apples", 4);
-console.log(cart);
+// import add, { cart } from "./shoppingCart.js";
+// add("pizza", 2);
+// add("bread", 5);
+// add("apples", 4);
+// console.log(cart);
 
+// !
 // console.log("start fetching");
 // const res = await fetch("https://jsonplaceholder.typicode.com/posts");
 
@@ -26,18 +27,48 @@ console.log(cart);
 // console.log(data);
 // console.log("Something");
 
-const getLastPost = async function () {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await res.json();
-  //   console.log(data);
-  return { title: data.at(-1).title, text: data.at(-1).body };
-};
+// const getLastPost = async function () {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//   const data = await res.json();
+//   //   console.log(data);
+//   return { title: data.at(-1).title, text: data.at(-1).body };
+// };
 
-const lastPost = getLastPost();
-console.log(lastPost);
+// const lastPost = getLastPost();
+// console.log(lastPost);
 
-// Not very clean
-// lastPost.then((last) => console.log(last));
+// // Not very clean
+// // lastPost.then((last) => console.log(last));
 
-const lastpost2 = await getLastPost();
-console.log(lastpost2);
+// const lastpost2 = await getLastPost();
+// console.log(lastpost2);
+// !
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to the cart (shipping cost is ${shippingCost})`
+    );
+  };
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+  return {
+    addToCart,
+    orderStock,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart("apple", 4);
+ShoppingCart2.addToCart("pizza", 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
