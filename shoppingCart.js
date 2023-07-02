c52d652153b0b759c49e6448e30bfbc956fcc9a1
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2023-07-03 02:48:57 +0330

    modified:   index.js 	modified:   shoppingCart.js

--- shoppingCart.js
+++ shoppingCart.js
@@ -2,6 +2,11 @@
 
 console.log("Exporting module");
 
+// Blocking code
+// console.log("Start fetching users");
+// await fetch("https://jsonplaceholder.typicode.com/users");
+// console.log("Finish fetching");
+
 const shippingCost = 10;
 export const cart = [];
 
